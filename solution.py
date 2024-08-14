72d6b8ff032594efaa5fb11bc12838127354bc71
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2024-08-14 11:44:04 +1000

    Update solution.py

--- solution.py
+++ solution.py
@@ -37,7 +37,6 @@ class Solver:
         visited = {}
         heapify(queue)
         while len(queue):
-            # print(queue[0][2]
             cost, history_actions, state = heappop(queue)
             if self.environment.is_solved(state):
                 print(history_actions)
@@ -80,21 +79,26 @@ class Solver:
         :return a real number h(n)
         """
         widget_cells = [widget_get_occupied_cells(self.environment.widget_types[i], state.widget_centres[i],state.widget_orients[i]) for i in range(self.environment.n_widgets)]
-        value = 0
+        value = 1.5*len(self.environment.target_list)
         max_distance = 0
+        min_distance = 100
         for tgt in self.environment.target_list:
             # loop over all widgets to find a match
             for i in range(self.environment.n_widgets):
-                if tgt not in widget_cells[i]:
+                if tgt in widget_cells[i]:
                     # match found
-                    value += 1.5
+                    value -= 1.5
+                    break
         for wd in state.widget_centres:
-            x=abs(wd[0]-state.BEE_posit[0])
-            y=abs(wd[1]-state.BEE_posit[1])
-            distance = x+y-1
+            distance = abs(wd[0]-state.BEE_posit[0])+abs(wd[1]-state.BEE_posit[1])-1
             if distance > max_distance:
                 max_distance = distance
-        value += max_distance
+            if distance < min_distance:
+                min_distance = distance
+        if max_distance==min_distance:
+            value += max_distance
+        else:
+            value += max_distance+min_distance
         return value
 
     def solve_a_star(self):
@@ -102,13 +106,12 @@ class Solver:
         Find a path which solves the environment using A* search.
         :return: path (list of actions, where each action is an element of BEE_ACTIONS)
         """
-        # Data format: tuple (cost+heuristic,cost,history_actions,state)
-        queue = [(self.compute_heuristic(self.environment.get_init_state()),0,[],self.environment.get_init_state())]
+        # Data format: tuple (cost+heuristic,history_actions,cost,state)
+        queue = [(self.compute_heuristic(self.environment.get_init_state()),[],0,self.environment.get_init_state())]
         visited = {}
         heapify(queue)
         while len(queue):
-            # print(queue[0][2]
-            totalcost,cost, history_actions, state = heappop(queue)
+            totalcost,history_actions,cost, state = heappop(queue)
             if self.environment.is_solved(state):
                 print(history_actions)
                 return history_actions
@@ -121,7 +124,7 @@ class Solver:
                 visited[state]=cost
                 for next_state in state.get_successor():
                     newcost=cost + next_state[1]
-                    heappush(queue,(self.compute_heuristic(next_state[2])+newcost,newcost,history_actions + [next_state[3]],next_state[2]))
+                    heappush(queue,(self.compute_heuristic(next_state[2])+newcost,history_actions + [next_state[3]],newcost,next_state[2]))
         pass
 
     #
